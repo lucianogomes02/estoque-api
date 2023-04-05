4accf098c5bb1517d3e5b1a9c6e32df6bd5c3ce0
lucianogomes02 <luciano@pontotel.com.br>
2023-04-04 22:53:06 -0300

    feat(produto/models.py): cria modelo Produto + migration

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+from enum import Enum
+
+
+class TipoDeProduto(Enum):
+    KIT: str = "Kit"
+    BARRA: str = "Barra"
+    TRUFA: str = "Trufa"
+    EVENTO: str = "Evento"
+
+    @classmethod
+    def opcoes(cls):
+        return (
+            ("KIT", cls.KIT.value),
+            ("BARRA", cls.BARRA.value),
+            ("TRUFA", cls.TRUFA.value),
+            ("EVENTO", cls.EVENTO.value),
+        )
+
+
+class Produto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome = models.CharField(
+        max_length=80,
+        blank=False,
+        null=False,
+    )
+    tipo = models.CharField(
+        max_length=10,
+        choices=TipoDeProduto.opcoes(),
+        blank=False,
+        null=False,
+        default="BARRA",
+    )
+
+    def __str__(self):
+        return f""
