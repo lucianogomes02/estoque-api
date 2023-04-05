f9d239316698cafa9f883436dd8ccfae57d16c24
lucianogomes02 <luciano@pontotel.com.br>
2023-04-05 15:53:26 -0300

    feat(produto/serializer.py): implementa Serializer e ViewSet de Produto

---
+++ produto/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from produto.models import Produto
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = "__all__"
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 
-# Create your views here.
+from produto.models import Produto
+from produto.serializers import ProdutoSerializer
+
+
+class ProdutosViewSet(viewsets.ModelViewSet):
+    """API de Produtos"""
+
+    queryset = Produto.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = ["nome"]
+    search_fields = ["nome", "cpf"]
+    serializer_class = ProdutoSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data.get("id"))
+            response["Location"] = request.build_absolute_uri() + id
+            return response
